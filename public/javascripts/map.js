4fb2df0b4c2a0040502a484df67f1464ff3e7da4
franco <fcedo13@gmail.com>
2020-08-28 16:10:04 -0300

    Api local terminada con bicicletas en mapa , Modulo 1

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -3,3 +3,16 @@ var map = L.map('main_map').setView([-32.98624, -68.87269], 13);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+//llamada a ajax. Reques asincronico , solicitud a web json
+
+$.ajax({
+    dataType: "json",
+    url: "api/bicicletas",
+    success: function(result){
+        console.log(result);
+        result.bicicletas.forEach(function(bici){
+            L.marker(bici.ubicacion, {title: bici.id}).addTo(map);
+        });
+    }
+})
--- routes/api/bicicletas.js
+++ routes/api/bicicletas.js
@@ -3,5 +3,8 @@ var router = express.Router();
 var bicicletaController = require('../../controllers/api/bicicletaControllerAPI');
 
 router.get('/', bicicletaController.bicicleta_list);
+router.post('/create', bicicletaController.bicicleta_create);
+router.delete('/delete', bicicletaController.bicicleta_delete);
+
 
 module.exports = router;
