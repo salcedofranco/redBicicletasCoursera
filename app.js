c0438b07d66228e9e8e4e95a9b3093d2b6faed9d
franco <fcedo13@gmail.com>
2020-09-08 12:28:46 -0300

    Agregamos vistas de login y passport, y cargamos en app.js, instalamos dependencias passport

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const passport = require("./config/passport");
+const session = require("express-session");
 
 var indexRouter = require('./routes/index');
 var tokenRouter = require("./routes/token");
@@ -11,7 +13,16 @@ var bicicletasRouter = require('./routes/bicicletas');
 var bicicletasAPIRouter = require('./routes/api/bicicletas');
 var usuariosAPIRouter = require('./routes/api/usuarios');
 
+const store = new session.MemoryStore;
+
 var app = express();
+app.use(session({
+  cookie: { maxAge: 240 * 60 * 60 * 1000 },
+    store: store,
+    saveUninitialized: true,
+    resave: "true",
+    secret: 'red_bicis_!!!***!".!.!.123123',
+}));
 
 //mongoose
 var mongoose = require('mongoose');
@@ -31,8 +42,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//rutas
 app.use('/', indexRouter);
 app.use("/usuarios", usersRouter);
 app.use('/bicicletas', bicicletasRouter);
@@ -41,6 +55,16 @@ app.use("/token", tokenRouter);
 //API
 app.use('/api/bicicletas', bicicletasAPIRouter);
 app.use('/api/usuarios', usuariosAPIRouter);
+
+
+//PASSPORT
+app.get('/login', function(req, res) {
+  res.render('session/login');
+});
+
+app.post('/login', function(req, res, next) {
+  //passport
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
