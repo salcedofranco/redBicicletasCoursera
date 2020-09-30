60218ba77418ed7d9e1ca805bc845ecf20e8756a
franco <fcedo13@gmail.com>
2020-09-30 11:47:03 -0300

    Sendgrid

--- mailer/mailer.js
+++ mailer/mailer.js
@@ -37,3 +37,45 @@ if ( environment === 'production'){
   
 
 module.exports = getMailTransport(process.env.NODE_ENV);
+
+/*
+const nodemailer = require('nodemailer');
+const sgTransport = require('nodemailer-sendgrid-transport');
+
+let mailConfig;
+
+if (process.env.NODE_ENV === 'production') {
+    const options = {
+        auth: {
+            api_key: process.env.SENDGRID_API_KEY
+        }
+    }
+
+    mailConfig = sgTransport(options);
+
+} else {
+    if (process.env.NODE_ENV === 'staging') {
+        const options = {
+            auth: {
+                api_key: process.env.SENDGRID_API_KEY
+            }
+        }
+
+        mailConfig = sgTransport(options);
+
+    } else {
+        // Development
+        mailConfig = {
+            host: 'smtp.ethereal.email',
+            port: 587,
+            auth: {
+                user: process.env.ETHEREAL_USER,
+                pass: process.env.ETHEREAL_PWD
+            }
+        };
+    }
+}
+
+module.exports = nodemailer.createTransport(mailConfig);
+
+*/
