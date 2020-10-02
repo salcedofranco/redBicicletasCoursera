f3da0b3ea370b68cdeb1a93ccc7a54fb8d0b0b4c
franco <fcedo13@gmail.com>
2020-10-02 16:13:16 -0300

    Sendgrid

--- mailer/mailer.js
+++ mailer/mailer.js
@@ -1,44 +1,3 @@
-const nodemailer = require("nodemailer");
-const sgTransport = require('nodemailer-sendgrid-transport');
-
-let mailConfig;
-
-const getMailTransport = ( environment ) => {
-if ( environment === 'production'){
-    const options = {
-      auth: {
-        api_key: process.env.SENDGRID_API_KEY
-      }
-    }
-    return nodemailer.createTransport(sgTransport(options)); 
-  }
-  
-  if (environment === 'staging' ){
-     
-      const options = {
-        auth: {
-          api_key: process.env.SENDGRID_API_KEY
-        }
-      }
-      return nodemailer.createTransport(sgTransport(options)); 
-  
-    } 
-      // all emails are catched by ethereal.email
-      mailConfig = {
-        host: 'smtp.ethereal.email',
-        port: 587,
-        auth: {
-          user: process.env.ETHEREAL_USER,
-          pass: process.env.ETHEREAL_PWD
-        }
-      }
-      return nodemailer.createTransport(mailConfig);
-    }
-  
-
-module.exports = getMailTransport(process.env.NODE_ENV);
-
-/*
 const nodemailer = require('nodemailer');
 const sgTransport = require('nodemailer-sendgrid-transport');
 
@@ -78,4 +37,3 @@ if (process.env.NODE_ENV === 'production') {
 
 module.exports = nodemailer.createTransport(mailConfig);
 
-*/
--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,30 +1,23 @@
-var map = L.map('main_map').setView([-32.98624, -68.87269], 13);
+var mymap = L.map('main_map').setView([-32.98624, -68.87269], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+}).addTo(mymap);
+
+//L.marker([3.4592355,-76.5048994], { title: 'CC. Unico Outlet' }).addTo(mymap); 
+//L.marker([-32.4787,-68.57894], { title: 'CC. Unico Outlet' }).addTo(mymap); 
 
 //llamada a ajax. Reques asincronico , solicitud a web json
-
 $.ajax({
-    method: 'POST',
     dataType: 'json',
-    url: 'api/auth/authenticate',
-    data: { email: 'fcedo13clash@gmail.com', password: 'keko' },
-}).done(function( data ) {
-    console.log(data);
+    url: 'api/bicicletas',
+    beforeSend: function (xhr) {
+        xhr.setRequestHeader("x-access-token", data.data.token);
+    }
+}).done(function (result) {
+    console.log(result);
 
-    $.ajax({
-        dataType: 'json',
-        url: 'api/bicicletas',
-        beforeSend: function (xhr) {
-            xhr.setRequestHeader("x-access-token", data.data.token);
-        }
-    }).done(function (result) {
-        console.log(result);
-
-        result.bicicletas.forEach(bici => {
-            L.marker(bici.ubicacion, { title: bici.id }).addTo(map);
-        });
+    result.bicicletas.forEach(bici => {
+        L.marker(bici.ubicacion, { title: bici.id }).addTo(mymap);
     });
 });
